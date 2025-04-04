a2d25ad8145e6567e78e56dc0185fe768b34a527
oleksandr <olehandrmukhalkuk@gmail.com>
2025-04-04 22:10:19 +0300

    Програма в якій потрібно вгадати випадкове число від 0 до 10 з 3 спроб з підказками в вигляді менше чи більше і якщо ви не відгадали то показує загадане число Prohrama v y

---
+++ Dz_2_3.py
@@ -0,0 +1,16 @@
+import random
+print("Вам потрібно відгадати випадкове число від 1 до 10 за 3 спроби")
+random_number = random.randint(1, 10)
+count = 0
+user_number = 0
+while count<=3:
+  user_number = int(input('Ваше число: '))
+  count +=1
+  if random_number > user_number:
+    print("Більше")
+  elif random_number < user_number:
+    print("Менше")
+  elif random_number == user_number:
+    print("Ви відгадали число!")
+  if count==3:
+    print(f"Ви не відгадали число. Було загадано:", random_number)

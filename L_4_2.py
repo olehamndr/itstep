4710f5dbe2856b45eed1584b3ea6487690cd897c
oleksandr <olehandrmukhalkuk@gmail.com>
2025-04-13 11:16:47 +0300

    L_4_2

--- L_4_2.py
+++ L_4_2.py
@@ -1,9 +1,23 @@
 class student:
-    print("Hello world")
-
-    def __init__(self, height=164):
+    number = 0
+    def __init__(self, height=164, name = None):
         self.height = height
-Artem = student(height=170)
-Mark = student()
-print("My height = ", Artem.height)
-print("My height = ", Mark.height)
+        student.number += 1
+        self.name = name
+
+    def grow(self, height=3):
+        self.height+=height
+
+    def __str__(self):
+        return f"Я студент з іменем {self.name} мій зріст {self.height}"
+Artem = student(height=170, name = "Artem")
+Mark = student(name = "Mark")
+print(Artem)
+print(Mark)
+
+while Mark.height<=180:
+    Mark.grow()
+
+print(Artem)
+print(Mark)
+print("number of student", student.number)

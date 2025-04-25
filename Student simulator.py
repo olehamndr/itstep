489ba0271045fa0b72b5f1e80dec94b298f06a2a
oleksandr <olehandrmukhalkuk@gmail.com>
2025-04-25 20:39:37 +0300

    Клас Student моделює життя студента з такими атрибутами:
    name — ім’я студента,
    
    gladness — рівень щастя,
    
    progress — рівень успішності у навчанні,
    
    money — кількість грошей,
    
    alive — стан студента (чи він ще продовжує навчання).
    
    Методи класу:
    
    to_study — імітує навчання: збільшує успішність, але зменшує щастя;
    
    to_work — імітує роботу: додає гроші, але зменшує щастя;
    
    to_sleep — імітує сон: трохи підвищує щастя;
    
    to_chill — імітує відпочинок: підвищує щастя, але зменшує успішність і гроші;
    
    is_alive — перевіряє, чи студент ще навчається: завершує симуляцію, якщо рівень успішності, грошей або щастя досягає критичного рівня;
    
    end_of_day — виводить інформацію про стан студента наприкінці кожного дня;
    
    live(day) — моделює поведінку студента протягом одного дня залежно від його стану.

--- Student simulator.py
+++ Student simulator.py
@@ -4,18 +4,24 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 100
         self.alive = True
     def to_study(self):
         print("Time to study")
         self.progress += 0.12
         self.gladness -= 5
+    def to_work(self):
+        print("Time to work")
+        self.gladness -= 4
+        self.money += 50
     def to_sleep(self):
         print("I will sleep")
         self.gladness += 3
     def to_chill(self):
         print("Rest time")
-        self.gladness += 5
+        self.gladness += 10
         self.progress -= 0.1
+        self.money -= 50
     def is_alive(self):
         if self.progress < -0.5:
             print("Cast out...")
@@ -26,24 +32,37 @@ class Student:
         elif self.progress > 5:
             print("Passed externally...")
             self.alive = False
+        elif self.money < -100:
+            print("Bankrupt...")
+            self.alive = False
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {round(self.progress, 2)}")
+        print(f"Money = {self.money}")
     def live(self, day):
-        day = "Day" + str(day) + "of" + self.name + "life"
+        day = "Day " + str(day) + " of " + self.name + " life"
         print(f"{day:=^50}")
-        live_cube = random.randint(1, 3)
-        if live_cube == 1:
+        live_cube = random.randint(1, 4)
+        if self.money < 50:
+            self.to_work()
+        elif self.progress < 1:
             self.to_study()
-        elif live_cube == 2:
-            self.to_sleep()
-        elif live_cube == 3:
+        elif self.gladness < 10:
             self.to_chill()
+        else:
+            if live_cube == 1:
+                self.to_study()
+            elif live_cube == 2:
+                self.to_sleep()
+            elif live_cube == 3:
+                self.to_chill()
+            elif live_cube == 4:
+                self.to_work()
         self.end_of_day()
         self.is_alive()
 
 nick = Student(name="Nick")
-for day in range(365):
-    if nick.alive == False:
+for day in range(1, 366):
+    if not nick.alive:
         break
-nick.live(day)
+    nick.live(day)

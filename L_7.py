6e9891cbc4d13e19a733a79c0188a5b5c077c682
oleksandr <olehandrmukhalkuk@gmail.com>
2025-04-27 10:48:35 +0300

    L_7

--- L_7.py
+++ L_7.py
@@ -5,9 +5,22 @@ class human:
 class Car:
     def __init__(self, brand="Porsche"):
         self.brand = brand
-        self.passagers = []
+        self.passengers = []
+
+    def add_passengers(self, human):
+        self.passengers.append(human)
+
+    def print_passengers(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers: ")
+            for i in self.passengers:
+                print(i.name)
 
 dima = human("Dmytro")
-ihor = human("Ihor")
+oleksandr = human("Oleksandr")
 car1 = Car("Lamborghini")
 car2 = Car()
+car2.add_passengers(dima)
+car1.add_passengers(oleksandr)
+car1.print_passengers()
+car2.print_passengers()
